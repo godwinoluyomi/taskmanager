7f8ee8f60f0672e3463251b87a84c1ead0e920fa
Godwin Oluyomi <godwinoluyomi@gmail.com>
2024-01-19 10:59:39 +0100

    deploy1

--- server/index.js
+++ server/index.js
@@ -5,19 +5,22 @@ const cors = require("cors");
 const connectDB = require("./src/config/db"); // Import the connectDB
 const authRoutes = require("./src/routes/authRoutes"); // Import auth routes
 const taskRoutes = require("./src/routes/taskRoutes"); // Import task routes
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
-/* app.use(
+// app.use(cors());
+app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     withCredentials: true,
     methods: ["GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE"],
   })
-); */
+);
 app.use(bodyParser.json());
 // app.use(express.json()); // Can be used instead of body-parser.
 
